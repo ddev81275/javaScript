346d330a70bb856efcc9d3f747a77259ee2cc9dc
almasmendybayev <almasmendybayev@tengizchevroil.com>
2022-08-22 16:55:25 +0600

    Adding Books foulder

---
+++ Book/Chapter 2.4.js
@@ -0,0 +1,15 @@
+let i;
+let my_value;
+let _my_private_value;
+let $dollarValue;
+
+var let; //Вне класса
+// const let = 100; //not working
+
+let from; let set; let target;
+
+let as, async = 10;
+
+let имя = "Tom";
+
+console.log(имя);
