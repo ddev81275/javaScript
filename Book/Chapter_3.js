91d225f6dcfd786ef95c68b01749e7aeff48b472
almasmendybayev <almasmendybayev@tengizchevroil.com>
2022-08-22 19:03:52 +0600

    .replace(), strings

--- Book/Chapter_3.js
+++ Book/Chapter_3.js
@@ -35,10 +35,26 @@ let PI_VALUE = Math.PI;
 console.log('E: ' + Math.E);
 console.log('INFITINY :>> ', Number.POSITIVE_INFINITY);*/
 
-console.log('------------------------');
+// console.log('------------------------');
 
 let dateNow = Date.now();
 const date = new Date();
 
-ShowObjValue(dateNow);
-ShowObjValue(date);
+/*ShowObjValue(dateNow);
+ShowObjValue(date);*/
+
+console.log('------------------------');
+
+let greet = 
+'Hello \
+User';
+const nameOfCompany = 'Hello Jh\'on!';
+const smileInUnicode = '\u{1F600}';
+const carModel = 'Nissan X/125 GT-1';
+const nissanModel = carModel.replace('125', '75');
+
+// console.log('nameOfCompany :>> ', nameOfCompany);
+// console.log('smile: ' + smileInUnicode);
+// console.log(greet);
+console.log(carModel);
+console.log(nissanModel);
