8971fab99cb41e758917ece64aebed4db82b76da
almasmendybayev <almasmendybayev@tengizchevroil.com>
2022-08-25 12:14:37 +0600

    Objects

---
+++ Book/Chapter_4.js
@@ -0,0 +1,17 @@
+const my_array = [1, 2, 3];
+const simple_obj = {
+    sub_name: 'simple',
+    model: '15C'
+};
+const my_obj = {
+    name: 'obj',
+    year: 2022,
+    coordinates: {
+        x: 1,
+        y: 2
+    }
+};
+
+console.log(my_array);
+console.log(simple_obj);
+console.log(my_obj);
