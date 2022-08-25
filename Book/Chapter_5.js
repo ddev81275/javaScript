710e5b734dfdabb9f554fc6f680c0b370c530a30
almasmendybayev <almasmendybayev@tengizchevroil.com>
2022-08-25 17:32:15 +0600

    for of

--- Book/Chapter_5.js
+++ Book/Chapter_5.js
@@ -29,14 +29,22 @@ const numberForIf = 10;
 //     console.log(_whileCounter);
 //     _whileCounter++;
 // }
-console.log('--------------------\nfor');
-for (let iter = 1; iter < 12; iter = iter + 2) {
-    console.log(iter);
-}
+// console.log('--------------------\nfor');
+// for (let iter = 1; iter < 12; iter = iter + 2) {
+//     console.log(iter);
+// }
 
-let i, j, sum = 0;
-for (i = 0, j = 0; i < 10; i++, j++) {
-    sum += i * j;
-    console.log(sum);
+// let i, j, sum = 0;
+// for (i = 0, j = 0; i < 10; i++, j++) {
+//     sum += i * j;
+//     console.log(sum);
+// }
+// console.log('summa =' + sum);
+console.log('--------------------\nfor of');
+const _forOfArray = [1, 2, 3, 4, 5, 6, 12, 78, 100];
+let sumForOf = 0;
+for (const iterator of _forOfArray) {
+    sumForOf += iterator;
 }
-console.log('summa =' + sum);
+console.log(typeof (sumForOf));
+console.log(sumForOf);
