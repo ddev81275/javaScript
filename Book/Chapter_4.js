60cff0c24783c3b1f99b14a187ecec74eb53b28a
almasmendybayev <almasmendybayev@tengizchevroil.com>
2022-08-25 12:25:47 +0600

    get by props by dot/['prop_name']

--- Book/Chapter_4.js
+++ Book/Chapter_4.js
@@ -1,3 +1,6 @@
+let Sum2Nums = function (a, b) {
+    return a + b;
+}
 const my_array = [1, 2, 3];
 const simple_obj = {
     sub_name: 'simple',
@@ -9,9 +12,24 @@ const my_obj = {
     coordinates: {
         x: 1,
         y: 2
-    }
+    },
+    sum_2_nums: Sum2Nums
 };
+const _first = {
+    name: 'first',
+    sub_name: '1'
+};
+const _second = {
+    name: 'second',
+    sub_name: '2'
+};
+const _third = [_first, 1, 2, [7878, 8989]];
+// console.log(_first.name);
+// console.log(_second['name']);
 
-console.log(my_array);
-console.log(simple_obj);
-console.log(my_obj);
+// console.log(my_array);
+// console.log(simple_obj);
+// console.log(my_obj);
+
+console.log(_third[0].name);
+// console.log(_third[8].name); // TypeError: Cannot read properties of undefined (reading 'name')
